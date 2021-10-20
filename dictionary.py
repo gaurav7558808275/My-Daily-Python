d29ed46d870695a821b9b5550210f4675bf81945
gaurav phadke <grvgaurav03@gmail.com>
2021-10-20 20:03:30 +0530

    Dictionaries completed

--- dictionary.py
+++ dictionary.py
@@ -4,7 +4,7 @@
 #no duplicates, changeable
 
 
-mydict = {"date":  '14',
+mydict = {"date"  :  '14',
           "month" : 'january',
           "year"  :  '1996'}
 
@@ -25,6 +25,23 @@ print(mydict.values())
 mydict["era"] = "someshit"
 print(mydict)
 
+#returns in a tuple
+x =(mydict.items())
+print(x)
+    
+#check is key exist or not
 
+if "date" in mydict:
+    print("avaiable")
+print(mydict.keys)
 
+#pop() function removes the key and data
+(mydict.pop("date"))
+print(mydict)
+#del in python can be used to delete specific elements
+del mydict["year"]
+#del mydict
 
+#using clear dict
+mydict.clear()
+print(mydict) #error should arise 
