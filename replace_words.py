f4018eed1504531a02e35cf55625d8a8b043e2ee
gaurav phadke <grvgaurav03@gmail.com>
2021-11-10 11:27:29 +0530

    1hr challenge done

---
+++ replace_words.py
@@ -0,0 +1,7 @@
+#replace the characer
+
+str = "my name is kumar gaurav I want to become a pythonista"
+str = str.replace("gaurav","Gaurav")
+str = str.replace('m','M',1)
+#str1 =''.join([str[i] for str in range(len(str)) if i!=2])
+print(str)
