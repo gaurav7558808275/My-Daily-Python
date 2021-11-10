85f377dd63809cead0c0e1f7bca6e1305630a57c
gaurav phadke <grvgaurav03@gmail.com>
2021-11-10 10:06:49 +0530

    update

--- swap.py
+++ swap.py
@@ -6,11 +6,21 @@ def swap(data):
     data[length -1] = t
 
     return data
-    
+
+def swap2(data):
+    data[0],data[-1] = data[-1],data[0]
+    return data
+
+def swap3(data):
+    r = data[0],data[-1]
+    data[-1],data[0] = r
+    return data
 
 
-
-
-data = [10,11,12,13,14]
-r = swap(data)
+data1 = [10,11,12,13,14]
+data2 = [10,11,12,13,14]
+data3 = [10,11,12,13,14]
+r = swap(data1)
+print(swap2(data2))
+print(swap3(data3))
 print(r)
