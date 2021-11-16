06ce4e78fb76f3bf94b00c4bff69844f7a02de78
gaurav phadke <grvgaurav03@gmail.com>
2021-11-16 14:00:38 +0530

    built-in function

--- Datetime/datetime_1.py
+++ Datetime/datetime_1.py
@@ -14,3 +14,29 @@ print(x.day)
 # G ISO year 
 
 print(x.strftime("%f"))
+
+#abs() real and imaginary is present it retrun the real part
+print(abs(22.22))
+#aiter()
+
+#all() retrun truw if all the iterables return true.
+
+list = [1,2,3,4,5]
+print(str(all(list)))
+# ascii of the object
+str = "gaurav + is"
+print(ascii(str))
+
+# bin() starts with 0b to indcate starts with binary
+print(bin(2))
+print(bin(-2))
+
+#bool() if false it returns false otherwise true.
+
+#bytrearray(source,encoding,error) syntax
+str = "gaurav is great"
+print(str.encode())
+str1 = bytearray(str,'utf-8')
+str2 = bytearray(str,'utf-16')
+print(str1)
+print(str2)
