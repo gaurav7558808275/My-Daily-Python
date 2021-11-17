c246764ef968b7bf0ff23607f28b1d5a9e0a262e
gaurav phadke <grvgaurav03@gmail.com>
2021-11-17 23:06:53 +0530

    data abstraction

---
+++ OOP/data_abstraction.py
@@ -0,0 +1,9 @@
+#data abstraction is another setup of the classes 
+
+from inheritance_1 import date
+
+class data(date):
+    def __init__(self,x):
+        self.date_print = x
+
+# you can use the classs from another library, in other words it can be abstacted.
