de4c4ebda158e1a911a7980645f282d72633fc5d
gaurav phadke <grvgaurav03@gmail.com>
2021-11-27 16:19:48 +0530

    Tata is not the end its the beginning

---
+++ Problems/linear_regression.py
@@ -0,0 +1,7 @@
+import pandas as p
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+data = {'area' : [2000,3000,3500,4000,4500], 'price'  : [4000,4500,5000,5600,5800]}
+dataframe = pandas.DataFrame(data)
